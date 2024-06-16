2fcf29fb497850a486938febd83b18209fd8e6c6
Syed Ali Jawad <jawad.bukhari1511@gmail.com>
2024-06-16 14:53:16 +0500

    Navbar Menu completed, Menu Popup left

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,36 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    maxWidth: {
+      container: "1440px",
+      contentContainer: "1140px",
+      containerSmall: "1024px",
+      containerxs: "786px",
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      screens: {
+        xs: "320px",
+        sm: "375px",
+        sml: "500px", // Small Landscape
+        md: "667px",
+        mdl: "768px", // Medium Landscape
+        lg: "960px",
+        lgl: "1024px", // Large Landscape
+        xl: "1280px",
+      },
+      fontFamily: {
+        bodyFont: ["Montserrat", "sans-serif"],
+        titleFont: ["Inter", "sans-serif"],
+      },
+      boxShadow: {
+        navbarShadow: "0 10px 30px -10px rgba(2,12,27,0.7)",
+      },
+      colors: {
+        bodyColor: "#0A192F",
+        textGreen: "#64FFDA",
+        textLight: "#CCD6F6",
+        textDark: "#8892B0",
+        hoverColor: "rgba(100,255,218,0.1)",
       },
     },
   },
